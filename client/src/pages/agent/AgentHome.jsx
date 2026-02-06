bf08cbfec0faa46a393082fbfb1b981076689000
homegroupmedia4-commits <homegroupmedia4@gmail.com>
2026-02-06 21:40:43 +0000

    Add AffgentfOthverProdsuctsBlock + save fotherSdefflections

--- client/src/pages/agent/AgentHome.jsx
+++ client/src/pages/agent/AgentHome.jsx
@@ -1425,20 +1425,20 @@ next.categorieId = selectedCategoryId;
         <div className="agenthome-block">
        
 
-          {/* <button
+          <button
             className="agenthome-btn"
             type="button"
             onClick={submitPdf}
             disabled={savingPdf}
           >
             {savingPdf ? "Génération..." : "Valider & Générer PDF"}
-          </button> */}
+          </button> 
 
-          {/* {pdfUrl ? (
+           {pdfUrl ? (
             <a className="agenthome-pdf" href={pdfUrl} target="_blank" rel="noreferrer">
               Ouvrir le PDF
             </a>
-          ) : null} */}
+          ) : null}
         </div>
 
         {error ? <div className="agenthome-error">{error}</div> : null}
