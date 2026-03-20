0aaaa6d1373b336ef93965c838ea14ca8117fe06
homegroupmedia4-commits <homegroupmedia4@gmail.com>
2026-03-20 11:17:29 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,6 +37,12 @@ app.use(
 app.use(express.json());
 app.use(express.static("public"));
 
+// ✅ AJOUTE CES 4 LIGNES ICI
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+
 app.use((req, res, next) => {
   console.log("➡️", req.method, req.originalUrl);
   next();
