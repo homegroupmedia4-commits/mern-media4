e26db6487c8769bdab807322f85b782b46f2df85
homegroupmedia4-commits <homegroupmedia4@gmail.com>
2026-04-28 10:47:43 +0200

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true, unique: true, maxlength: 80 },
-    systemKey: { type: String, trim: true, lowercase: true, default: "", unique: true, sparse: true },
+   systemKey: {
+  type: String,
+  trim: true,
+  lowercase: true,
+  default: null,
+  unique: true,
+  sparse: true,
+},
     isActive: { type: Boolean, default: true },
   },
   { timestamps: true }
